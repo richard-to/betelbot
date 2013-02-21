0fc81f31488b74e8454588ab0dfeeca308df9b36
richard-to <gohn76@gmail.com>
2013-02-20 17:00:04 -0900

    Fix a few bugs in teleop console

--- betelbot/console-teleop-client.py
+++ betelbot/console-teleop-client.py
@@ -1,10 +1,9 @@
 from socket import socket
 sock = socket()
 sock.connect(("127.0.0.1", 8888))
-sock.send("SDF\r\n")
 while 1:
-    data = raw_input("Enter data: ")
-    if (data):
-        sock.send(data + "\r\n")
+    data = raw_input("Enter move: ")
+    if data:
+        sock.send("publish betelbot_move {}\n".format(data))
     else:
         break
