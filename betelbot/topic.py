5e88f479771cd10f4e70d9d4237207d8c609e9b5
richard-to <gohn76@gmail.com>
2013-02-25 02:17:12 -0900

    Add MoveTopic class to validate messages

--- betelbot/topic.py
+++ betelbot/topic.py
@@ -1,3 +1,15 @@
+class MoveTopic:
+
+    dataType = ('1', '2', '3', '4')
+
+    @staticmethod
+    def isValid(data):
+        if len(data) == 1:
+            return data[0] in MoveTopic.dataType
+        else:
+            return False
+
+
 msgs = {
-    'move': ['1', '2', '3', '4']
+    'move': MoveTopic
 }
