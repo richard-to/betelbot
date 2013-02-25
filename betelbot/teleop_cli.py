8e937c3e672f6483b111c2348aab957ad25512e8
richard-to <gohn76@gmail.com>
2013-02-25 01:34:20 -0900

    Use NonBlockingTerm in teleop_cli

--- betelbot/teleop_cli.py
+++ betelbot/teleop_cli.py
@@ -1,40 +1,38 @@
 import ConfigParser
-import fcntl
-import logging
-import os
-import select
-import signal
-import sys 
-import termios
+import sys
 import threading
-import time
-import tty 
 
 from tornado.ioloop import IOLoop
 
 from topic import msgs
-from util import PubSubClient, signal_handler
+from util import PubSubClient, NonBlockingTerm
 
 
 def threadedLoop():
     IOLoop.instance().start()
  
 
-def callback(data=None):
+def onMovePublished(data=None):
     if data:
         print data.strip()
 
 
-def isData():
-    return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], [])
+def onInput(client, validMoves):
+    c = sys.stdin.read(1)
+    if c == '\x1b':
+        try:
+            c = sys.stdin.read(2)
+            if c in validMoves:
+                client.publish('move', validMoves[c])
+        except: pass
 
 
 def main():
     config = ConfigParser.SafeConfigParser()
     config.read('config/default.cfg')
     client = PubSubClient('', config.getint('server', 'port'))
-    client.subscribe('move', callback)
-    
+    client.subscribe('move', onMovePublished)
+
     validMoves = {
         '[A': '1',
         '[B': '2',
@@ -42,32 +40,12 @@ def main():
         '[D': '4'
     }
 
-    t = threading.Thread(target=threadedLoop)
-    t.daemon = True
-    t.start()
+    thread = threading.Thread(target=threadedLoop)
+    thread.daemon = True
+    thread.start()
 
-    signal.signal(signal.SIGINT, signal_handler)
-    
-    old_settings = termios.tcgetattr(sys.stdin)
-
-    fd = sys.stdin.fileno()
-    fl = fcntl.fcntl(fd, fcntl.F_GETFL)
-    fcntl.fcntl(fd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
-
-    try:
-        tty.setcbreak(sys.stdin.fileno())
-        while True:
-            time.sleep(.3)
-            if isData():
-                c = sys.stdin.read(1)
-                if c == '\x1b':
-                    try:
-                        c = sys.stdin.read(2)
-                        if c in validMoves:
-                            client.publish('move', validMoves[c])
-                    except: continue
-    finally:
-        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+    term = NonBlockingTerm()
+    term.run(lambda: onInput(client, validMoves))
 
 if __name__ == "__main__":
     main()
