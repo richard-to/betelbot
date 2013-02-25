b5cc4e20afca961528e2489495293ab1c90e73df
richard-to <gohn76@gmail.com>
2013-02-25 01:32:51 -0900

    Add Keyboard interrupt handler

--- betelbot/log_cli.py
+++ betelbot/log_cli.py
@@ -3,7 +3,7 @@ import logging
 
 from tornado.ioloop import IOLoop
 
-from util import PubSubClient
+from util import PubSubClient, signal_handler
 from topic import msgs
 
 
@@ -13,13 +13,15 @@ def callback(data=None):
 
 
 def main():
+    signal.signal(signal.SIGINT, signal_handler)
+
     config = ConfigParser.SafeConfigParser()
     config.read('config/default.cfg')
     client = PubSubClient('', config.getint('server', 'port'))
     for msg in msgs:
         client.subscribe(msg, callback)
+    IOLoop.instance().start()
 
 
 if __name__ == "__main__":
     main()
-    IOLoop.instance().start()
