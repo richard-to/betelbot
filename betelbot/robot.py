9bf89b8980d6d9c1e70494e01c7e657a08c2ccd3
richard-to <gohn76@gmail.com>
2013-03-12 01:16:39 -0800

    Clean up betelbot driver code

--- betelbot/robot.py
+++ betelbot/robot.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import ConfigParser
 import logging
 import os
@@ -13,37 +15,38 @@ from tornado.netutil import TCPServer
 from topic import msgs
 from util import PubSubClient, signal_handler
 
-topics = msgs
-topicNames = dict((key,[]) for key in msgs.keys())
-
-subcribers = []
-
 
 class BetelBotDriver(TCPServer):
  
-    def __init__(self, io_loop=None, ssl_options=None, **kwargs):
-        logging.info('BetelBot Driver is running')
+    def __init__(self, client, io_loop=None, ssl_options=None, **kwargs):
+        logging.info('BetelBot Driver is running')        
         TCPServer.__init__(self, io_loop=io_loop, ssl_options=ssl_options, **kwargs)
- 
+        self.subscribers = []
+        self.client = client
+
     def handle_stream(self, stream, address):
-        BetelBotDriverConnection(stream, address)
+        BetelBotDriverConnection(stream, address, self.client)
 
 
 class BetelBotDriverConnection(object):
  
     streamSet = set([])
 
-    def __init__(self, stream, address):
-        subcribers.append(self)
-        self.stream = stream
+    def __init__(self, stream, address, client):
+        self._logInfo('BetelBot connected')
         self.address = address
-        self.streamSet.add(self.stream)
+
+        self.stream = stream        
         self.stream.set_close_callback(self._onClose)
         self.stream.read_until('\n', self._onReadLine)
-        self._logInfo('BetelBot connected')
- 
+        self.streamSet.add(self.stream)
+
+        self.client = client
+        self.client.subscribe(CmdTopic.id, self._onCmdForBot)
+
     def write(self, data):
-        self.stream.write(data, self._onWriteComplete);
+        self.stream.write(data, self._onWriteComplete)
+
     def _onReadLine(self, data):
         self._logInfo('Received data')
         if not self.stream.reading():
@@ -62,25 +65,26 @@ class BetelBotDriverConnection(object):
         dt = datetime.now().strftime("%m-%d-%y %H:%M")
         logging.info('[%s, %s]%s', self.address[0], dt, msg)
 
+    def _onCmdForBot(self, topic, data=None):
+        self.write(data[0])
 
 
-def callback(topic, data=None):
-    for c in subcribers:
-        c.write(data[0])
+def main():
+    signal.signal(signal.SIGINT, signal_handler)
+
+    config = ConfigParser.SafeConfigParser()
+    config.read('config/default.cfg')
+
+    logger = logging.getLogger('')
+    logger.setLevel(config.get('general', 'log_level'))
+
+    client = PubSubClient('', config.getint('server', 'port'))
+
+    server = BetelBotDriver(client, msgs)
+    server.listen(config.getint('robot', 'port'))
+
+    IOLoop.instance().start()
 
 
-config = ConfigParser.SafeConfigParser()
-config.read('config/default.cfg')
-
-logger = logging.getLogger('')
-logger.setLevel(config.get('general', 'log_level'))
-
-signal.signal(signal.SIGINT, signal_handler)
-
-server = BetelBotDriver()
-server.listen(config.getint('robot', 'port'))
-
-client = PubSubClient('', config.getint('server', 'port'))
-client.subscribe('move', callback)
-
-IOLoop.instance().start()
+if __name__ == '__main__':
+    main()
