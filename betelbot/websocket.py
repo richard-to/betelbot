5bba2c4b59b9aff35e1294aa3554ceeda517fcf3
richard-to <gohn76@gmail.com>
2013-03-12 00:14:52 -0800

    Clean up websocket code to not use globals

--- betelbot/websocket.py
+++ betelbot/websocket.py
@@ -1,51 +1,54 @@
 import ConfigParser
 import logging
+import signal
 import socket
 
 from tornado.iostream import IOStream
 from tornado.ioloop import IOLoop
 from tornado import web, websocket
 
-from util import PubSubClient
-from topic import msgs
+from util import PubSubClient, signal_handler
+from topic import HistogramTopic
 
-connections = []
-
-
-subscribers = []
 
 class VizualizerWebSocket(websocket.WebSocketHandler):
+    
+    def initialize(self, client):
+        self.client = client
+        self.
 
     def open(self):
-        logging.info('WebSocket connected.')
-        connections.append(self)
+        logging.info('WebSocket connected. Subscribing to histogram topic.')
+        client.subscribe(HistogramTopic.id, self.callback)
 
     def on_message(self, message):
         self.write_message(message)
-        if message == 'subscribe histogram':
-            subscribers.append(self)
 
     def on_close(self):
         logging.info('WebSocket closed.')
-        connections.remove(self)
+
+    def callback(self, topic, data=None):
+        self.write_message(" ".join(map(str, data)))
 
 
-def callback(topic, data=None):
-    print data
-    for s in subscribers:
-        s.write_message(" ".join(map(str, data)))
+def main():
+    connections = []
+    subscribers = []
+
+    signal.signal(signal.SIGINT, signal_handler)
+
+    config = ConfigParser.SafeConfigParser()
+    config.read('config/default.cfg')
+
+    client = PubSubClient('', config.getint('server', 'port'))
+
+    application = web.Application([
+        (r"/socket", VizualizerWebSocket, dict(client=client)),
+    ])
+
+    application.listen(config.getint('websocket-server', 'port'))
+    IOLoop.instance().start()
 
 
-config = ConfigParser.SafeConfigParser()
-config.read('config/default.cfg')
-
-client = PubSubClient('', config.getint('server', 'port'))
-client.subscribe('histogram', callback)
-
-application = web.Application([
-    (r"/socket", VizualizerWebSocket),
-])
-
-application.listen(config.getint('websocket-server', 'port'))
-
-IOLoop.instance().start()
+if __name__ == '__main__':
+    main()
