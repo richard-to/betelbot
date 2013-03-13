543ecdafc4ec33350c97ee1d708561f7426b5358
richard-to <gohn76@gmail.com>
2013-03-12 23:08:42 -0800

    Fix bugs in Histogragram1D example
    Forward should be 'k', not 1
    Can't use 10-13 pins with WiFly shield. 9 also?
    Move some logic out of client.isavailable method.

--- Arduino/libraries/Betelbot/examples/Histogram1D/Histogram1D.ino
+++ Arduino/libraries/Betelbot/examples/Histogram1D/Histogram1D.ino
@@ -7,9 +7,8 @@
 #include <Betelbot.h>
 #include "settings.h"
 
-#define PING_SWEEP_DELAY 425
-
-#define KEY_FORWARD '1'
+#define PING_SWEEP_DELAY 450
+#define KEY_FORWARD 'k'
 
 const char* ssid = SSID;
 const char* pass = PASS;
@@ -18,17 +17,17 @@ const char* serverAddress = SERVER;
 const int serverPort = PORT;
 
 int resolution = 24;
-int radius = 3.2;
+float radius = 3.2;
 int boundB = 20;
-int boundW = 125;
+int boundW = 40;
 
 int irPin = 0;
 
-int pingPin = 7;
-int pingServoPin = 11;
+int pingPin = 6;
+int pingServoPin = 5;
 
-int leftservoPin = 9;
-int rightservoPin = 10;
+int leftservoPin = 2;
+int rightservoPin = 3;
 
 Servo pingServo;
 Servo rightservo;
@@ -40,21 +39,17 @@ PingTurret pingTurret;
 
 WiFlyClient client;
 
-
 bool isRunning = false;
-int revolutions = 0;
 
 void setup() {
-  Serial.begin(9600);
-
   WiFly.begin();
   WiFly.join(ssid, pass, true);
-  client.connect(serverAddress,serverPort);
+  client.connect(serverAddress, serverPort);
 
   pingServo.attach(pingServoPin);
   pingTurret.begin(pingServo, pingPin, PING_SWEEP_DELAY);
   pingTurret.scanRight();
-
+  
   rightservo.attach(rightservoPin);
   leftservo.attach(leftservoPin);
   driver.begin(leftservo, rightservo);
@@ -66,20 +61,22 @@ void setup() {
 void loop() {
   char c;
   if (client.available()) {
-    c = client.read();  
-    if (c == KEY_FORWARD && isRunning == false) {
-      isRunning = true;
-      encoder.run();
-      driver.forward();
-    }
-    if (isRunning == true) {
-      if (encoder.status() == ENCODER_ENCODE) {
-        encoder.encode();
-      } else {
-        isRunning = false;
-        driver.stop();
-        client.println(pingTurret.scan());
-      }
-    }  
+    c = client.read();
   }
+  
+  if (c == KEY_FORWARD && isRunning == false) {
+    isRunning = true;
+     encoder.run();
+     driver.forward();
+  }
+  
+  if (isRunning == true) {
+    if (encoder.status() == ENCODER_ENCODE) {
+      encoder.encode();
+    } else {
+      isRunning = false;
+      driver.stop();
+      client.println(pingTurret.scan());
+    }
+  }  
 }
