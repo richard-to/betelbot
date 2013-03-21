fd6cad2eb484648787d3810264d8ec820aa04f47
richard-to <gohn76@gmail.com>
2013-03-21 00:47:45 -0800

    Save particle filter code
    Not in working state yet

--- betelbot/particle.py
+++ betelbot/particle.py
@@ -5,18 +5,35 @@ import logging
 import random
 import signal
 
+from math import *
+
+import numpy as np
+
 from tornado.ioloop import IOLoop
 
 from map import simple_world
 from topic import histogramTopic, moveTopic, senseTopic
-from util import PubSubClient, signal_handler
+from util import PubSubClient, signalHandler, loadGridData
 
+
+max_steering_angle = pi / 4.0 # You do not need to use this value, but keep in mind the limitations of a real car.
+bearing_noise = 0.1 # Noise parameter: should be included in sense function.
+steering_noise = 0.1 # Noise parameter: should be included in move function.
+distance_noise = 5.0 # Noise parameter: should be included in move function.
+
+tolerance_xy = 15.0 # Tolerance for localization in the x and y directions.
+tolerance_orientation = 0.25 # Tolerance for orientation.
 
 class Particle:
-    
-    def __init__(self, length = 20.0):
-        self.x = random.random() * world_size
-        self.y = random.random() * world_size
+
+    def __init__(self, grid, lookupTable):
+        self.grid = grid
+        self.lookupTable = lookupTable
+        while True;
+            self.x = random.randint(0, len(grid[0]) - 1)
+            self.y = random.randint(0, len(grid) - 1)
+            if grid[x][y] > 0:
+                break
         self.orientation = random.random() * 2.0 * pi
         self.length = length
         self.bearing_noise  = 0.0
@@ -24,7 +41,6 @@ class Particle:
         self.distance_noise = 0.0
 
     def set(self, new_x, new_y, new_orientation):
-
         if new_orientation < 0 or new_orientation >= 2 * pi:
             raise ValueError, 'Orientation must be in [0..2pi]'
         self.x = float(new_x)
@@ -70,7 +86,7 @@ class Particle:
             orientation = (self.orientation + beta + random.gauss(0.0, self.steering_noise)) % (2 * pi)
 
         result = Particle(self.length)
-        result.set(x, y, orientation)
+        result.set(int(x), int(y), orientation)
         result.set_noise(self.bearing_noise, self.steering_noise, self.distance_noise)
         return result
     
@@ -90,17 +106,29 @@ class ParticleFilter:
     def __init__(self, client):
         self.client = client
 
+def loadGridData(filename):
+    file = open(filename, 'r')
+    data = file.readlines()
+    return np.array(json.loads(data[0]))
+
 
 def main():
-    signal.signal(signal.SIGINT, signal_handler)
+    #signal.signal(signal.SIGINT, signal_handler)
 
-    config = ConfigParser.SafeConfigParser()
-    config.read('config/default.cfg')
+    #config = ConfigParser.SafeConfigParser()
+    #config.read('config/default.cfg')
 
-    client = PubSubClient('', config.getint('server', 'port'))
-    particleFilter = ParticleFilter(client)
+    # client = PubSubClient('', config.getint('server', 'port'))
+    # particleFilter = ParticleFilter(client)
 
-    IOLoop.instance().start()
+    # IOLoop.instance().start()
+    grid = loadGridData(''.join([config.get('map', 'dir'), config.get('map-data', 'grid')]))
+    lookupTbl = [ 
+        loadGridData(''.join([config.get('map', 'dir'), config.get('map-data', 'ldmap')])),
+        loadGridData(''.join([config.get('map', 'dir'), config.get('map-data', 'rdmap')])),
+        loadGridData(''.join([config.get('map', 'dir'), config.get('map-data', 'udmap')])),
+        loadGridData(''.join([config.get('map', 'dir'), config.get('map-data', 'ddmap')]))
+    ]
 
 if __name__ == '__main__':
     main()
