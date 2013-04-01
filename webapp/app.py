13ca355977805f68b3d488a413027a8ce4239755
richard-to <gohn76@gmail.com>
2013-03-31 19:09:48 -0800

    Rename web-visualizer folder to webapp

--- webapp/app.py
+++ webapp/app.py
@@ -2,21 +2,30 @@ import os
 from flask import Flask, render_template
 from config import default
 
+
 app = Flask(__name__)
-app.config.from_object(default)
+app.config.from_object(default)  
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/canvas')
 def canvas():
     return render_template('canvas.html')
+
 
 @app.route('/test')
 def test():
     return render_template('test.html')
 
-if __name__ == '__main__':
+
+def main():  
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
+
+if __name__ == '__main__':
+    main()
