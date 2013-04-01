90b38852bd1a16d74206317082ef4e3eb86530d4
richard-to <gohn76@gmail.com>
2013-04-01 15:40:03 -0800

    Remove unused topics. Load topic dictionary using reflection helpers

---
+++ betelbot/topic/__init__.py
@@ -0,0 +1,32 @@
+from util import loadMsgDictFromPkg
+
+
+class ValueTopic(object):
+    # Base topic that is used to validate parameters 
+    # against a fixed set of values.
+    #
+    # This class should be considered abstract.
+    # 
+    # Topic classes should have constructors with that 
+    # accept no parameters. This constraint makes it 
+    # easier to dynamically load and instantiage classes.
+    #
+    # Additionally topics should be immutable since there 
+    # can be multiple instances of the same Topic.
+    
+    def __init__(self, id, allowedValues, numParams=1):
+        self.id = id
+        self.allowedValues = allowedValues
+        self.numParams = numParams
+
+    def isValid(self, data):
+        if len(data) == self.numParams:
+            return all(value in self.allowedValues for value in data)
+        else:
+            return False
+
+
+def getTopics():
+    # Loads all topics into dictionary with topic id as key.
+
+    return loadMsgDictFromPkg(__file__)
---
+++ betelbot/topic/default.py
@@ -0,0 +1,21 @@
+class CmdTopic(ValueTopic):
+    # Command msgs contain directions for Betelbot to move. These messages
+    # are usually published by a controller interface.
+    #
+    # Valid movements are left, down, up, right, and stop.
+    #
+    # Vi controls are used to avoid multibyte arrow keys.
+    #
+    # CommandTopic is slightly different than the MoveTopic.
+    
+    def __init__(self):
+        super(CmdTopic, self).__init__('cmd', ('h', 'j', 'k', 'l', 's'), 1)
+
+
+class MoveTopic(CmdTopic):
+    # The MoveTopic differs from the CmdTopic in that this topic is 
+    # published by the Robot after it completes its move.
+    
+    def __init__(self):
+        super(MoveTopic, self).__init__()
+        self.id = 'move'
