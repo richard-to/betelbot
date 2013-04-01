9d3af8d5a8c181bc37b7456400a483d60ef85513
richard-to <gohn76@gmail.com>
2013-03-31 20:00:21 -0800

    Add comments to NonBlockingTerm

--- betelbot/util.py
+++ betelbot/util.py
@@ -20,8 +20,25 @@ def signalHandler(signal, frame):
 
 
 class NonBlockingTerm:
+    # Makes it so terminal handles key input asynchronously.
+    # 
+    # The benefit is that we don't have to depend on readline, 
+    # which will block until input is recieved. This prevents 
+    # the terminal from receiving and outputting messages that may 
+    # be sent by Betelbot server.
+    #
+    # This implementation only works in Linux and Mac since it uses OS specific 
+    # functions. This has only been tested on a Mac.
 
     def run(self, cb):
+        # This method starts the non-blocking terminal.
+        # 
+        # A callback must be passed in to handle user key input. It will be called 
+        # for every key press.
+        #
+        # This implementation does not handle multi-byte characters, such as 
+        # arrow keys. A previous version of this code did handle this case.
+
         signal.signal(signal.SIGINT, signalHandler)
         
         old_settings = termios.tcgetattr(sys.stdin)
@@ -40,6 +57,8 @@ class NonBlockingTerm:
             termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
 
     def hasData(self):
+        # This method checks if we have any input from user.
+
         return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], [])
 
 
