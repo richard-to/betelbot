9fa7a02414fda01ac3f1058d10a0495653b70a19
richard-to <gohn76@gmail.com>
2013-04-04 01:41:09 -0800

    Add locate and request path from path finder

--- betelbot/robosim.py
+++ betelbot/robosim.py
@@ -4,24 +4,52 @@ import ConfigParser
 import logging
 import random
 import signal
+import time
 
 from tornado.ioloop import IOLoop
 
 from client import BetelbotClientConnection
+from pathfinder import PathfinderMethod
 from topic.default import CmdTopic, MoveTopic
 from util import Client, signalHandler
 
 
 class RoboSim(object):
 
-    def __init__(self, conn):
+    def __init__(self, conn, start, goal, delay=5):
+        self.delay = delay
+        self.start = start
+        self.goal = goal
         self.cmdTopic = CmdTopic()
         self.moveTopic = MoveTopic()
         self.conn = conn
         self.conn.subscribe(self.cmdTopic.id, self.onCmdPublished)
+        self.conn.locate(1, PathfinderMethod.SEARCH, self.onLocateResponse)
 
     def move(self, direction):
         self.conn.publish(self.moveTopic.id, direction)
+
+    def onLocateResponse(self, found=False):
+        if found:
+            self.conn.search(self.onSearchResponse, 1, self.start, self.goal)
+
+    def onSearchResponse(self, id, method, result):
+        delta = {
+            'k': [-1, 0], 
+            'h': [0, -1], 
+            'j': [1, 0],
+            'l': [0, 1]
+        }
+        current = self.start
+        moves = result[0][1:]
+        for move in moves:
+            temp = [move[0] - current[0], move[1] - current[1]]
+            for key in delta:
+                if delta[key] == temp:
+                    self.move(key)
+                    break
+            current = move
+            time.sleep(self.delay) 
 
     def onCmdPublished(self, topic, data=None):
         self.move(data[0])
@@ -35,11 +63,14 @@ def main():
 
     logger = logging.getLogger('')
     logger.setLevel(config.get('general', 'log_level'))
-    
+
+    start = [int(num) for num in config.get('map', 'start').split(',')]
+    goal = [int(num) for num in config.get('map', 'goal').split(',')]
+
     client = Client('', config.getint('server', 'port'), BetelbotClientConnection)
     conn = client.connect()
 
-    roboSim = RoboSim(conn)
+    roboSim = RoboSim(conn, start, goal)
     IOLoop.instance().start()
 
 
