519848cb9666f6f441793af54453c0ed43c399b0
richard-to <gohn76@gmail.com>
2013-04-06 21:05:18 -0800

    Fix bugs and get algorithm working moderately well.
    Code not in usable state yet. Contains debugging code

--- betelbot/particle.py
+++ betelbot/particle.py
@@ -16,6 +16,7 @@ from tornado.ioloop import IOLoop
 from client import BetelbotClientConnection
 from util import Client, signalHandler
 
+
 class Particle:
 
     def __init__(self, length, grid, lookupTable):
@@ -23,9 +24,9 @@ class Particle:
         self.lookupTable = lookupTable
         self.delta = [[1, 0], [0, 1], [1, 0], [0, 1]]
         self.length = length
-        self.bearingNoise  = 0.0
-        self.steeringNoise = 0.0
-        self.distanceNoise = 0.0
+        self.forwardNoise  = 0.0
+        self.turnNoise = 0.0
+        self.senseNoise = 0.0
         self.y = 0.0
         self.x = 0.0
 
@@ -46,64 +47,85 @@ class Particle:
         self.y = float(y)
         self.orientation = float(orientation)
 
-    def setNoise(self, bNoise, sNoise, dNoise):
-        self.bearingNoise  = float(bNoise)
-        self.steeringNoise = float(sNoise)
-        self.distanceNoise = float(dNoise)
+    def setNoise(self, fNoise, tNoise, sNoise):
+        self.forwardNoise  = float(fNoise)
+        self.turnNoise = float(tNoise)
+        self.senseNoise = float(sNoise)
 
     def measurementProb(self, measurements):
-        predictedMeasurements = self.sense(0)
-        error = 1.0
-        for i in range(len(measurements)):
-            error_bearing = abs(measurements[i] - predictedMeasurements[i])
-            error_bearing = (error_bearing + pi) % (2.0 * pi) - pi
-
-            error *= (exp(- (error_bearing ** 2) / (self.bearingNoise ** 2) / 2.0) /  
-                      sqrt(2.0 * pi * (self.bearingNoise ** 2)))
-        return error
+        prob = 1.0
+        if (self.y >= 0 and self.y < self.grid.shape[0] and
+            self.x >= 0 and self.x < self.grid.shape[1] and
+            self.grid[self.y][self.x] > 0):
+            count = len(self.delta) 
+            index = self.y * self.grid.shape[1] * count + self.x * count
+            for i in xrange(count):
+                value = self.lookupTable[index]
+                dy = value * self.delta[i][0]
+                dx = value * self.delta[i][1]
+                index += 1
+                dist = dy or dx
+                prob *= self.gaussian(float(dist), self.senseNoise, float(measurements[i]))
+        else:
+            prob = 0
+        return prob
     
     def move(self, motion):
-        theta, d = motion 
-        if d < 0:
-            raise ValueError, 'Robot cant move backwards'
-        d = float(d) + random.gauss(0.0, self.distanceNoise)
-        theta = float(theta)   
-        beta = (d/self.length) * tan(theta)
-        if(abs(beta) < 0.001):
-            orientation = (self.orientation + random.gauss(0.0, self.steeringNoise)) % 2.0 * pi            
-            x = self.x + d * cos(orientation)
-            y = self.y + d * sin(orientation)
-        else:
-            radius = d/beta        
-            cx = float(self.x) - sin(self.orientation) * radius
-            cy = float(self.y) + cos(self.orientation) * radius  
-            x = cx + sin(self.orientation + beta) * radius
-            y = cy - cos(self.orientation + beta) * radius
-            orientation = (self.orientation + beta + random.gauss(0.0, self.steeringNoise)) % 2.0 * pi
-        result = Particle(self.length, self.grid, self.lookupTable)
-        result.set(round(x), round(y), orientation)
-        result.setNoise(self.bearingNoise, self.steeringNoise, self.distanceNoise)
-        return result
+        turn, forward = motion
+
+        if forward < 0:
+            raise ValueError, 'Robot cant move backwards'         
+        
+        # turn, and add randomness to the turning command
+        orientation = self.orientation + float(turn) + random.gauss(0.0, self.turnNoise)
+        orientation %= 2 * pi
+        
+        dist = float(forward) + random.gauss(0.0, self.forwardNoise)
+        x = self.x + (round(cos(orientation), 15) * dist)
+        y = self.y + (round(sin(orientation), 15) * dist)
+        y %= self.grid.shape[0]
+        x %= self.grid.shape[1]
+        particle = Particle(self.length, self.grid, self.lookupTable)
+        particle.set(round(y), round(x), orientation)
+        particle.setNoise(self.forwardNoise, self.turnNoise, self.senseNoise)
+        return particle
     
     def sense(self, hasNoise=False):
-        Z = []
-        count = len(self.delta) 
-        index = self.y * self.grid.shape[1] * count + self.x * count
-        for i in xrange(count):
-            value = self.lookupTable[index]
-            dy = value * self.delta[i][0]
-            dx = value * self.delta[i][1]
-            dist = (atan2(dy, dx) - self.orientation)
-            if hasNoise:
-                dist += random.gauss(0.0, self.bearingNoise)
-            dist %= 2.0 * pi
-            index += 1
-            Z.append(dist)
+        if self.grid[self.y][self.x] and self.grid[self.y][self.x] > 0:
+            Z = []
+            count = len(self.delta) 
+            index = self.y * self.grid.shape[1] * count + self.x * count
+            for i in xrange(count):
+                value = self.lookupTable[index]
+                dy = value * self.delta[i][0]
+                dx = value * self.delta[i][1]
+                index += 1
+                Z.append(dy or dx)
+        else:
+            Z = [inf, inf, inf, inf]
         return Z
+
+    def gaussian(self, mu, sigma, x):
+        return exp(- ((mu - x) ** 2) / (sigma ** 2) / 2.0) / sqrt(2.0 * pi * (sigma ** 2))
 
     def __repr__(self):
         return '[y=%.6s x=%.6s orient=%.6s]' % (str(self.y), str(self.x), 
                                                 str(self.orientation))
+
+
+def getPosition(p):
+    x = 0.0
+    y = 0.0
+    orientation = 0.0
+    for i in range(len(p)):
+        x += p[i].x
+        y += p[i].y
+        # orientation is tricky because it is cyclic. By normalizing
+        # around the first particle we are somewhat more robust to
+        # the 0=2pi problem
+        orientation += (((p[i].orientation - p[0].orientation + pi) % (2.0 * pi)) 
+                        + p[0].orientation - pi)
+    return [y / len(p), x / len(p), orientation / len(p)]
 
 
 class ParticleFilter:
@@ -114,13 +136,13 @@ class ParticleFilter:
         self.grid = grid
         self.lookupTable = lookupTable
 
-    def makeParticles(self, bearingNoise, steeringNoise, distanceNoise, N=None):
+    def makeParticles(self, forwardNoise, turnNoise, senseNoise, N=None):
         self.N = self.N if N is None else N
         self.particles = []
         for i in xrange(self.N):
             p = Particle(self.length, self.grid, self.lookupTable)
             p.randomizePosition()
-            p.setNoise(bearingNoise, steeringNoise, steeringNoise)
+            p.setNoise(forwardNoise, turnNoise, senseNoise)
             self.particles.append(p)        
     
     def getData(self):
@@ -131,12 +153,10 @@ class ParticleFilter:
         for i in xrange(self.N):
             updatedParticles.append(self.particles[i].move(motion))
         self.particles = updatedParticles
-
         weight = []
         for i in range(self.N):
             weight.append(self.particles[i].measurementProb(measurements))
-
-        self.particles = self.resample(self.particles, weight, self.N)  
+        self.particles = self.resample(self.particles, weight, self.N) 
 
     def resample(self, particles, weight, N):
         sampledParticles = []
@@ -151,6 +171,62 @@ class ParticleFilter:
             sampledParticles.append(particles[index])
         return sampledParticles       
 
+def moves(directions):
+    motions = []
+    circle = {
+        'l': 0, 
+        'j': pi/2, 
+        'h': pi, 
+        'k': 3 * pi / 2
+    }
+    test = {
+        'l': 0, 
+        'j': 1, 
+        'h': 2, 
+        'k': 3
+    }
+    prev = directions.pop(0)
+    for d in directions:
+        rot = 0
+        dist = test[d] - test[prev]
+        
+        if dist == 0:
+            rot = 0
+        elif dist == 1 or dist == -3:
+            rot = pi/2
+        elif dist == 2 or dist == -2:
+            rot = pi
+        elif dist == 3 or dist == -1:
+            rot = -pi/2
+        
+        motions.append([rot, 20.])
+        prev = d
+    return motions
+
+def sensor(y, x, grid, lookupTable):
+    delta = [[1, 0], [0, 1], [1, 0], [0, 1]]    
+    Z = []
+    count = len(delta) 
+    index = y * 20 * grid.shape[1] * count + x * 20 * count
+    for i in xrange(count):
+        value = lookupTable[index]
+        dy = value * delta[i][0]
+        dx = value * delta[i][1]
+        Z.append(dy or dx)
+        index += 1
+    return Z
+
+def check_output(final_robot, estimated_position):
+
+    error_x = abs(final_robot.x - estimated_position[0])
+    error_y = abs(final_robot.y - estimated_position[1])
+    error_orientation = abs(final_robot.orientation - estimated_position[2])
+    error_orientation = (error_orientation + pi) % (2.0 * pi) - pi
+    correct = error_x < tolerance_xy and error_y < tolerance_xy \
+              and error_orientation < tolerance_orientation
+    return correct
+
+
 def main():
     signal.signal(signal.SIGINT, signalHandler)
 
@@ -162,12 +238,24 @@ def main():
 
     length = config.getint('robot', 'length')
 
-    bearingNoise = config.getfloat('particle', 'bearingNoise')
-    steeringNoise = config.getfloat('particle', 'steeringNoise')
-    distanceNoise = config.getfloat('particle', 'distanceNoise')
-
+    forwardNoise = config.getfloat('particle', 'forwardNoise')
+    turnNoise = config.getfloat('particle', 'turnNoise')
+    senseNoise = config.getfloat('particle', 'senseNoise')
     particleFilter = ParticleFilter(length, grid, lookupTable)
-    particleFilter.makeParticles(bearingNoise, steeringNoise, distanceNoise)
+    particleFilter.makeParticles(forwardNoise, turnNoise, senseNoise)
+
+    directions = [u'j', u'j', u'j', u'j', u'h', u'h', u'h', u'h', u'h', u'h', u'h', u'h', u'h', u'h', u'j', u'j', u'j', u'j', u'j', u'j', u'j', u'j', u'j', u'j', u'h', u'j', u'h']
+    path = [[0, 14], [1, 14], [2, 14], [3, 14], [4, 14], [4, 13], [4, 12], [4, 11], [4, 10], [4, 9], [4, 8], [4, 7], [4, 6], [4, 5], [4, 4], [5, 4], [6, 4], [7, 4], [8, 4], [9, 4], [10, 4], [11, 4], [12, 4], [13, 4], [14, 4], [14, 3], [15, 3], [15, 2]]
+    path.pop(0)
+
+    measurements = []
+    for y, x in path:
+        measurements.append(sensor(y, x, grid, lookupTable))
+    motions = moves(directions)
+
+    for i in range(len(motions)):
+        particleFilter.update(motions[i], measurements[i])
+    print getPosition(particleFilter.particles)
 
 if __name__ == '__main__':
     main()
