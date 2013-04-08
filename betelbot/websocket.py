204d2baf66f930c6f5bebc29037f2735714c0d67
richard-to <gohn76@gmail.com>
2013-04-07 17:45:48 -0800

    Update websocket to subscribe to pathTopic

--- betelbot/websocket.py
+++ betelbot/websocket.py
@@ -13,7 +13,7 @@ import jsonrpc
 
 from client import BetelbotClientConnection
 from util import Client, signalHandler
-from topic.default import ParticleTopic
+from topic.default import ParticleTopic, PathTopic
 
 
 class VizualizerWebSocket(websocket.WebSocketHandler):
@@ -21,11 +21,13 @@ class VizualizerWebSocket(websocket.WebSocketHandler):
     def initialize(self, conn):
         self.conn = conn
         self.particleTopic = ParticleTopic()
+        self.pathTopic = PathTopic()
         self.encoder = jsonrpc.Encoder()
 
     def open(self):
         logging.info('WebSocket connected. Subscribing to particle topic')
         self.conn.subscribe(self.particleTopic.id, self.onNotifySub)
+        self.conn.subscribe(self.pathTopic.id, self.onNotifySub)
 
     def on_message(self, message):
         self.write_message(message)
