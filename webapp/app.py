4f5f9d73fd5ff1eb0c4a833731743e43b85af799
richard-to <gohn76@gmail.com>
2013-04-13 20:11:04 -0800

    Remove canvas route

--- webapp/app.py
+++ webapp/app.py
@@ -12,12 +12,7 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/canvas')
-def canvas():
-    return render_template('canvas.html')
-
-
-def main():  
+def main():
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
 
