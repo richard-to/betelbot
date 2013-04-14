4eafc2fc6763a5a41ee55d4221d51ef8a9622d9c
richard-to <gohn76@gmail.com>
2013-04-13 16:38:59 -0800

    Add comments to JsonConfig

--- betelbot/jsonconfig.py
+++ betelbot/jsonconfig.py
@@ -2,6 +2,22 @@ import json
 
 
 class JsonConfig(object):
+    # Parses a json file and dynamically creates variables
+    # for config values
+    #
+    # Currently defaults to a file in the module using a
+    # relatively path. Not a good idea...
+    #
+    # Should probably not have a default and force user
+    # to pass in as command line arg or better yet load from an
+    # environment variable.
+    #
+    # The json file is only two levels deep.
+    # The first level is a section and the second level contain
+    # config values for the section.
+    #
+    # Config values can be any valid data. So you could have an object,
+    # which could translate to a dict when loaded, etc.
 
     DEFAULT_FILEPATH = 'config/config.json'
 
@@ -15,6 +31,8 @@ class JsonConfig(object):
 
 
 class JsonConfigSection(object):
+    # Parse data in a section and dynamically create
+    # variable for instance.
 
     def __init__(self, data):
         for key in data:
