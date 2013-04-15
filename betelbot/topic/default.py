0bb6c4957fe4b04de1456faa62701ced49c6702d
richard-to <gohn76@gmail.com>
2013-04-14 23:13:58 -0800

    Add new topics: Waypoint, RobotStatus, Power, Mode

--- betelbot/topic/default.py
+++ betelbot/topic/default.py
@@ -1,5 +1,46 @@
 from topic import ValueTopic
 
+
+class WaypointTopic(object):
+
+    def __init__(self):
+        self.id = 'waypoint'
+
+    def isValid(self, *data):
+        return True
+
+
+class RobotStatusTopic(object):
+
+    def __init__(self):
+        self.id = 'robotstatus'
+
+    def isValid(self, *data):
+        return True
+
+
+class PowerTopic(ValueTopic):
+
+    def __init__(self):
+
+        self.on = "on"
+        self.off = "off"
+        self.keys = (self.on, self.off)
+
+        super(PowerTopic, self).__init__('power', self.keys, 1)
+
+
+class ModeTopic(ValueTopic):
+
+    def __init__(self):
+
+        self.autonomous = "autonomous"
+        self.manual = "manual"
+        self.keys = (self.autonomous, self.manual)
+
+        super(ModeTopic, self).__init__('mode', self.keys, 1)
+
+
 class CmdTopic(ValueTopic):
     # Command msgs contain directions for Betelbot to move. These messages
     # are usually published by a controller interface.
