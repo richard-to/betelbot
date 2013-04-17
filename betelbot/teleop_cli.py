85b1fefb41953d65eb001b37854aa55a17c0eef8
richard-to <gohn76@gmail.com>
2013-04-17 15:19:11 -0800

    Move teleop logic to Teleop class. Update cli instructions.
    Also use topic factory. Add constants for key codes.

--- betelbot/teleop_cli.py
+++ betelbot/teleop_cli.py
@@ -9,7 +9,8 @@ import jsonrpc
 
 from client import BetelbotClientConnection
 from config import JsonConfig
-from topic.default import CmdTopic
+from robosim import RobotMethod
+from topic import getTopicFactory
 from util import NonBlockingTerm, Client
 
 
@@ -20,69 +21,118 @@ def threadedLoop():
     IOLoop.instance().start()
 
 
-def onCmdPublished(topic, data=None):
-    # Debugging callback to make test if commands are
-    # being received and published.
+class TeleopKey(object):
+    POWER = 'p'
+    MANUAL = 'm'
+    AUTONOMOUS = 'a'
+    RESET = 'r'
+    WAYPOINT = 'w'
+    INFO = 'i'
 
-    if data:
-        print '[{}]{}'.format(topic, ' '.join(data))
 
+class Teleop(object):
 
-def onInput(conn, cmdTopic):
-    # When terminal receives key input, this input is published
-    # if it matches the accepted values of the command topic.
+    MSG_SERVICES_READY =  "[Services registered. Teleop ready.]"
+    MSG_SERVICES_LOADING = "[Registering services. Please wait.]"
 
-    c = sys.stdin.read(1)
-    if cmdTopic.isValid(c):
-        conn.publish(cmdTopic.id, c)
-    elif c == 'p':
-        conn.power(None, 'on')
-    elif c == 'm':
-        conn.mode(None, 'manual')
-    elif c == 'a':
-        conn.mode(None, 'autonomous')
-    elif c == 'r':
-        conn.publish("location", 0, 14)
-    elif c == 'w':
-        conn.publish("waypoint", [0, 14], [15, 2])
-    elif c == 'u':
-        conn.robotstatus(None)
+    def __init__(self, conn, topics, location, waypoint):
+        self.conn = conn
+        self.topics = topics
+        self.location = location
+        self.waypoint = waypoint
+        self.ready = False
 
-def printInstructions(cmdTopic):
-    # Prints Betelbot control instructions to console
+    def run(self):
+        self.conn.subscribe(self.topics.cmd.id, self.onCmdPublished)
+        self.conn.batchLocate(self.onBatchLocateResponse,
+            [RobotMethod.POWER, RobotMethod.MODE, RobotMethod.STATUS])
 
-    print "Reading from keyboard"
-    print "---------------------------"
-    print "Use [{}, {}, {}, {}] to move and [{}] to stop.".format(
-            cmdTopic.left, cmdTopic.down, cmdTopic.up,
-            cmdTopic.right, cmdTopic.stop)
+    def printInstructions(self):
+        # Prints Betelbot control instructions to console
 
-def onBatchLocateResponse(found):
-    if found:
-        print "Services located"
+        cmdTopic = self.topics.cmd
+        print 'Keyboard Commands List'
+        print '-----------------------'
+        print '[{}] Gets information on robot status.'.format(TeleopKey.INFO)
+        print '[{}] Toggles robot power on/off.'.format(TeleopKey.POWER)
+        print '[{}] Switches to manual mode.'.format(TeleopKey.MANUAL)
+        print '[{}] Switches to autonomous mode.'.format(TeleopKey.AUTONOMOUS)
+        print '[{}] Resets robot to default location.'.format(TeleopKey.RESET)
+        print '[{}] Sets default waypoint.'.format(TeleopKey.WAYPOINT)
+        print '[{}, {}, {}, {}] Controls robot.'.format(
+            cmdTopic.left, cmdTopic.down, cmdTopic.up, cmdTopic.right)
+        print '[{}] Stops robot.'.format(cmdTopic.stop)
+
+    def onServiceResponse(self, method, result):
+        if result:
+            print '[{}]{}'.format(method, ' '.join(result))
+
+    def onCmdPublished(self, topic, data=None):
+        # Debugging callback to make test if commands are
+        # being received and published.
+
+        if data:
+            print '[{}]{}'.format(topic, ' '.join(data))
+
+    def onInput(self):
+        # When terminal receives key input, this input is published
+        # if it matches the accepted values of the command topic.
+
+        if self.ready is not True:
+            print Teleop.MSG_SERVICES_LOADING
+
+        conn = self.conn
+        topics = self.topics
+
+        c = sys.stdin.read(1)
+        if topics.cmd.isValid(c):
+            conn.publish(topics.cmd.id, c)
+        elif c == TeleopKey.POWER:
+            conn.robot_power(
+                lambda result: self.onServiceResponse(RobotMethod.POWER, result),
+                topics.power.on)
+        elif c == TeleopKey.MANUAL:
+            conn.robot_mode(
+                lambda result: self.onServiceResponse(RobotMethod.MODE, result),
+                topics.mode.manual)
+        elif c == TeleopKey.AUTONOMOUS:
+            conn.robot_mode(
+                lambda result: self.onServiceResponse(RobotMethod.MODE, result),
+                topics.mode.autonomous)
+        elif c == TeleopKey.RESET:
+            conn.publish(topics.location.id, *self.location)
+        elif c == TeleopKey.WAYPOINT:
+            conn.publish(topics.waypoint.id, *self.waypoint)
+        elif c == TeleopKey.INFO:
+            conn.robot_status(
+                lambda result: self.onServiceResponse(RobotMethod.STATUS, result))
+
+    def onBatchLocateResponse(self, found):
+        if found:
+            print Teleop.MSG_SERVICES_READY
+            self.ready = True
+
 
 def main():
     # Starts up a client connection to publish commands to Betelbot server.
-    #
-    # - Use h, j, k, and l to move left, down, up, and right respectively.
-    # - Use s to stop.
 
     cfg = JsonConfig()
-
-    cmdTopic = CmdTopic()
+    topics = getTopicFactory()
 
     client = Client('', cfg.server.port, BetelbotClientConnection)
     conn = client.connect()
-    conn.subscribe(cmdTopic.id, onCmdPublished)
-    conn.batchLocate(onBatchLocateResponse, ["power", "mode", "robotstatus"])
+
+    teleop = Teleop(conn, topics, cfg.teleop.location, cfg.teleop.waypoint)
+
     thread = threading.Thread(target=threadedLoop)
     thread.daemon = True
     thread.start()
 
-    printInstructions(cmdTopic)
+    teleop.printInstructions()
+    teleop.run()
 
     term = NonBlockingTerm()
-    term.run(lambda: onInput(conn, cmdTopic))
+    term.run(teleop.onInput)
 
 
 if __name__ == "__main__":
