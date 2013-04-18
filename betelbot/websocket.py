e7c820c2397f07b561d8bdbfba0c6395fb9123b9
richard-to <gohn76@gmail.com>
2013-04-17 17:06:16 -0800

    Fix typo in classname. Adding global state to keep track of clients

--- betelbot/websocket.py
+++ betelbot/websocket.py
@@ -1,5 +1,6 @@
  #!/usr/bin/env python
 
+import json
 import logging
 import signal
 import socket
@@ -16,11 +17,13 @@ from util import Client, signalHandler
 from topic import getTopicFactory
 
 
-class VizualizerWebSocket(websocket.WebSocketHandler):
+class VisualizerWebSocket(websocket.WebSocketHandler):
 
     # Websocket log messages
     LOG_CONNECTED = 'WebSocket connected. Subscribing to topics'
     LOG_CLOSED = 'WebSocket closed'
+
+    state = {}
 
     def initialize(self, conn):
         self.conn = conn
@@ -28,7 +31,8 @@ class VizualizerWebSocket(websocket.WebSocketHandler):
         self.encoder = jsonrpc.Encoder()
 
     def open(self):
-        logging.info(VizualizerWebSocket.LOG_CONNECTED)
+        logging.info(VisualizerWebSocket.LOG_CONNECTED)
+
         self.conn.subscribe(self.topics.particle.id, self.onNotifySub)
         self.conn.subscribe(self.topics.path.id, self.onNotifySub)
         self.conn.subscribe(self.topics.power.id, self.onNotifySub)
@@ -36,13 +40,24 @@ class VizualizerWebSocket(websocket.WebSocketHandler):
         self.conn.subscribe(self.topics.location.id, self.onNotifySub)
         self.conn.subscribe(self.topics.waypoint.id, self.onNotifySub)
 
+        for key in VisualizerWebSocket.state:
+            msg = self.encoder.notification(key, state[key])
+            self.write(msg)
+
     def on_message(self, message):
-        self.write_message(message)
+        data = json.loads(message)
+        method = data.get(jsonrpc.Key.METHOD, None)
+        params = data.get(jsonrpc.Key.PARAMS, None)
+
+        print method
 
     def on_close(self):
-        logging.info(VizualizerWebSocket.LOG_CLOSED)
+        logging.info(VisualizerWebSocket.LOG_CLOSED)
 
     def onNotifySub(self, topic, data=None):
+        if topic == self.topics.particle:
+            VisualizerWebSocket.state[topic] = data
+
         msg = self.encoder.notification(topic, data[0])
         self.write_message(msg)
 
@@ -59,7 +74,7 @@ def main():
     conn = client.connect()
 
     application = web.Application([
-        (cfg.websocketServer.socketUri, VizualizerWebSocket, dict(conn=conn)),
+        (cfg.websocketServer.socketUri, VisualizerWebSocket, dict(conn=conn)),
     ])
 
     application.listen(cfg.websocketServer.port)
