142e9cb340e89b28e84a1732dadf4a7dfe504be3
richard-to <gohn76@gmail.com>
2013-04-24 01:55:21 -0800

    Add code to get sensor values.

--- Android/Compass/src/betelbot/compass/CompassActivity.java
+++ Android/Compass/src/betelbot/compass/CompassActivity.java
@@ -2,22 +2,48 @@ package betelbot.compass;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.widget.TextView;
 
-public class CompassActivity extends Activity {
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 
+public class CompassActivity extends Activity implements SensorEventListener {
+
+	private SensorManager sensorManager;
+	private TextView label;
+	private float azimuth;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compass);
+        label = (TextView)findViewById(R.id.compassLabel);
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
     }
-
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.compass, menu);
-        return true;
+    protected void onResume() {
+    	super.onResume();
+    	sensorManager.registerListener(this,
+    			sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+    			SensorManager.SENSOR_DELAY_NORMAL);
     }
     
+    @Override
+    protected void onPause() {
+    	super.onPause();
+    	sensorManager.unregisterListener(this);
+    }
+    
+	@Override
+	public void onAccuracyChanged(Sensor arg0, int arg1) {}
+
+
+	@Override
+	public void onSensorChanged(SensorEvent arg0) {
+		azimuth = arg0.values[0];
+		label.setText(String.valueOf(azimuth));
+	}
 }
